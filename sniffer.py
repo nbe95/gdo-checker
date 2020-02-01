f8519cbaea1df2f21bb4ef13e17104b423ab2458
Niklas Bettgen <niklas@bettgen.de>
2020-02-01 11:25:50 +0100

    Initialer Commit. PDF-Links von URL extrahiert

---
+++ sniffer.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+import json
+
+CONFIG_FILE = "./config.json"
+
+
+def getContents(url):
+    page = urllib.request.urlopen(url)
+    return page
+
+
+def extractPdfs(content):
+    soup = BeautifulSoup(content, "html.parser")
+    main_content = str(soup.select("div#main")[0])
+
+    soup = BeautifulSoup(main_content, "html.parser")
+    links = soup.find_all("a")
+
+    pdfs = []
+    for l in links:
+        title = l.get_text()
+        href = l.get("href")
+        if href.endswith(".pdf"):
+            pdfs.append((title, href))
+    return pdfs
+
+
+def main():
+    with open(CONFIG_FILE) as f:
+        config = json.load(f)
+
+    content = getContents(config["url"])
+    files = extractPdfs(content)
+
+    print(files)
+
+
+if __name__ == "__main__":
+    main()

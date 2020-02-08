f60bb6ef6925c807b30454971afef918d5839a23
Niklas Bettgen <niklas@bettgen.de>
2020-02-08 10:10:45 +0100

    Parameter "WorkingDirectory" für Service-Modul nachgefügt.

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ exp=30      # days to keep old log files
 # Delete expired log files.
 find ./logs/ -name "*.log" -type f -mtime +$exp -delete
 
-# Run program and store output to log.
-./sniffer.py > ./logs/$stamp.log
+# Run program and additionally store output to log file.
+./sniffer.py | tee ./logs/$stamp.log

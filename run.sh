b22741cfc83f8604dc57532928c9999a4b0b7bc3
Niklas Bettgen <niklas@bettgen.de>
2020-02-09 12:34:12 +0100

    Hotfix: log-Verzeichnis wird erstellt, falls nicht vorhanden.

--- run.sh
+++ run.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # Set up configuration.
-stamp=`date '+%Y%m%d-%H%M'`
+stamp=$(date '+%Y%m%d-%H%M')
+log_path=./logs
 exp=30      # days to keep old log files
 
+
+# Create log directory if not yet existing.
+mkdir -p .$log_path
+
 # Delete expired log files.
-find ./logs/ -name "*.log" -type f -mtime +$exp -delete
+find $log_path -name "*.log" -type f -mtime +$exp -delete
 
 # Run program and additionally store output to log file.
-./sniffer.py | tee ./logs/$stamp.log
+./sniffer.py | tee $log_path/$stamp.log

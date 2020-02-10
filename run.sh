0534169c8387385af33d863ccf867818d0131f71
Niklas Bettgen <niklas@bettgen.de>
2020-02-10 13:34:51 +0100

    Parameter-Weitergabe an Python-Skript von Bash-Wrapper.

--- run.sh
+++ run.sh
@@ -7,10 +7,10 @@ exp=30      # days to keep old log files
 
 
 # Create log directory if not yet existing.
-mkdir -p .$log_path
+mkdir -p $log_path
 
 # Delete expired log files.
 find $log_path -name "*.log" -type f -mtime +$exp -delete
 
 # Run program and additionally store output to log file.
-./sniffer.py | tee $log_path/$stamp.log
+./sniffer.py $@ | tee $log_path/$stamp.log
